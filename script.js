babd7707558f40d764a40bf25e65c67555a1c31f
EdisonR2024 <164270204+EdisonR2024@users.noreply.github.com>
2024-10-23 13:02:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let principal = document.querySelector('main');
+
+principal.style.backgroundColor = "rgb(8, 102, 255)";
+
+let cabecera = document.querySelector('header');
+cabecera.setAttribute('style', 'color:rgb(185, 115, 238)');
+
+let circulo = document.getElementById('circulo');
+
+circulo.classList.add('circulo-verde');
+
+circulo.classList.add('circulo-amarillo');
